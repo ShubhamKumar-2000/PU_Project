6c3cf5e9333a53541df1ae8df7079efd474a8400
ShubhamKumar-2000 <kumarshuvam@gmail.com>
2023-01-03 00:15:33 +0530

    First Commit after successful connection and Testing of firebase

---
+++ app/src/main/java/com/example/trailconnect/CustomerLoginActivity.java
@@ -0,0 +1,111 @@
+package com.example.trailconnect;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class CustomerLoginActivity extends AppCompatActivity {
+
+    private EditText mEmail, mPassword;
+    private Button mLogin, mRegistration;
+    private FirebaseAuth mAuth;
+
+    //this is the listener for when Auth state changes
+    private FirebaseAuth.AuthStateListener firebaseAuthListener;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_login);
+
+        //below line will get the current state of Login status
+        mAuth = FirebaseAuth.getInstance();
+        firebaseAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            //whenever we Login or logout this AuthState Listener will be called
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                //this variable stores the information about the current logged in user
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                //I have this condition becoz this AuthStateListener is called even when user logout so we have to make sure user is loggedin in order to move forward
+                if(user != null){
+                    Intent intent = new Intent(CustomerLoginActivity.this, MapActivity.class);
+                    startActivity(intent);
+                    finish();
+                    return;
+                }
+
+            }
+        };
+
+        mEmail = findViewById(R.id.email);
+        mPassword = findViewById(R.id.password);
+        mLogin = findViewById(R.id.login);
+        mRegistration = findViewById(R.id.registration);
+
+        mRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = mEmail.getText().toString();
+                final String password = mPassword.getText().toString();
+                //below we are using mAuth to create a user with email and password
+                mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(CustomerLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(CustomerLoginActivity.this, "Sign up error", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            //here we are getting the user id that was assigned to the driver at the moment of registration
+                            String user_id = mAuth.getCurrentUser().getUid();
+                            //below database reference wll point to the Riders
+                            DatabaseReference current_user_db = FirebaseDatabase.getInstance().getReference().child("Users").child("Customers").child(user_id);
+                            current_user_db.setValue(true);
+                        }
+                    }
+                });
+            }
+        });
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = mEmail.getText().toString();
+                final String password = mPassword.getText().toString();
+                mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(CustomerLoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(!task.isSuccessful()){
+                            Toast.makeText(CustomerLoginActivity.this, "Sign in error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+    }
+
+    //below code means whenever this activity is called we are going to start the fireBaseAuthListener
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(firebaseAuthListener);
+    }
+    //this code will execute when we Leave this activity and at that time we are removing this firebaseAuth litener
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(firebaseAuthListener);
+    }
+}
